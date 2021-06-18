6a0cd270dd300b8540b5f4217a5133614342f441
EJ <ej.roux13@gmail.com>
2021-06-18 16:07:59 -0700

    updated nav tabs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React from "react";
 // Import Custom Logo
-import logo from "../../images/customLogo.png"
+import logo from "../../images/customLogo.png";
 // Import Font Awesome & Icon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 // Import Stylesheet
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
     return (
@@ -19,14 +19,17 @@ const Header = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
+                        </li> */}
                         <li className="nav-item">
                             <a className="nav-link active" href="#">About Me</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" href="#">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Skills</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" href="#">Experience</a>
