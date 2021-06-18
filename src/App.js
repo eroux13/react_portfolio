6b6d859220bd85c2aef8690e1ac62b6f9323cfec
EJ <ej.roux13@gmail.com>
2021-06-18 16:06:58 -0700

    added particles component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
 // Import Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 // Import Header Component
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+// Import Jumbotron Component
+import Jumbotron from './components/Jumbotron/Jumbotron';
+// Import Particles Component
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "square",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <Header/>
+      <Jumbotron/>
+    </>
   );
 }
 
